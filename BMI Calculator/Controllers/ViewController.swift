aa67f16e480f2b155fe5b2e35a2af8eddcc39db4
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-19 18:23:20 +0200

    silder matched the relevant label

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text =  "\(height) m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text =  "\(weight) Kg"
+    }
+    
+    
 }
 
